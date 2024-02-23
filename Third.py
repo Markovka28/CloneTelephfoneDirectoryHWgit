8d8fe0f6dd2037bd33d9462ba5323f21ca602bc7
Valery_Krylov <supermaaaan@mail.ru>
2024-02-23 12:57:16 +0500

    third_commit

---
+++ Third.py
@@ -0,0 +1,16 @@
+import random
+
+# Создание массива с буквами русского алфавита
+alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+
+# Функция для генерации случайного слова из заданной длины
+def generate_random_word(length):
+    random_word = ''
+    for _ in range(length):
+        random_word += random.choice(alphabet)
+    return random_word
+
+# Генерация и вывод случайных слов
+for _ in range(5):  # Генерируем 5 случайных слов
+    random_word = generate_random_word(random.randint(10, 50))  # Генерируем слова длиной от 5 до 10 символов
+    print(random_word)
